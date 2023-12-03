1b64ecbd9df21522525222cb6c60548de3b5a856
apoorv2766 <145976905+apoorv2766@users.noreply.github.com>
2023-12-03 14:27:05 +0530

    myportfolio

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import logo from "../assets/AG.png";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [nav, setnav] = useState(false);
+  const handleClick = () => setnav(!nav);
+
+  return (
+    <>
+      <div class="fixed w-full h-[60px] flex justify-between items-center px-4 bg-[black] text-gray-300">
+        <div>
+          <img
+            src={logo}
+            alt="my logo"
+            style={{ width: "80px", height:"60px" }}
+          />
+        </div>
+        {/* menu */}
+        <ul className="hidden md:flex text-xl">
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+        {/* hamburger */}
+        <div className="md:hidden z-10" onClick={handleClick}>
+          {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+        {/* mobile menu */}
+        <ul
+          className={
+            !nav
+              ? "hidden"
+              : "absolute top-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+          }
+        >
+          <li className="py-6 text-4xl">
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="skills"
+              smooth={true}
+              duration={500}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="contact"
+              smooth={true}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+        {/* social icon  */}
+        <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+          <ul>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+              <a
+                className=" flex justify-between items-center w-full text-gray-300"
+                href="https://www.linkedin.com/in/apoorv27"
+              >
+                Linkedin <FaLinkedin size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+              <a
+                className=" flex justify-between items-center w-full text-gray-300"
+                href="https://www.github.com/apoorv2766"
+              >
+                GitHub <FaGithub size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+              <a
+                className=" flex justify-between items-center w-full text-gray-300"
+                href="https://www.outlook.com/apoorv2766"
+              >
+                Email <HiOutlineMail size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+              <a
+                className=" flex justify-between items-center w-full text-gray-300"
+                href="https://www.dropbox.com/scl/fi/ap1vo56s669ow0fhr9998/latest_-resume.pdf?rlkey=n3xs288d2jetuktyn1z9tt7fl&dl=0"
+              >
+                Resume <BsFillPersonLinesFill size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/component/Work.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import myproject from "../assets/myproject.png";
+import tictactoe from "../assets/tictactoe.png";
+import quoteapi from "../assets/quoteapi.png";
+import todo from "../assets/todo.png";
+
+const Work = () => {
+  return (
+    <div name="work" className="bg-[#0a192f] w-full md:h-screen text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 pt-12 flex flex-col justify-center w-full h-full">
+        <div className="">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-4">//Check out some of my recent works</p>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 ">
+          {/* grid view */}
+          <div
+            style={{ backgroundImage: `url(${myproject})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white traking-wider">
+                ReactJS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* grid view */}
+          <div
+            style={{ backgroundImage: `url(${tictactoe})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white traking-wider">
+                ReactJS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* grid view */}
+          <div
+            style={{ backgroundImage: `url(${quoteapi})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white traking-wider">
+                ReactJS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* grid view */}
+          <div
+            style={{ backgroundImage: `url(${todo})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white traking-wider">
+                ReactJS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/apoorv2766">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
