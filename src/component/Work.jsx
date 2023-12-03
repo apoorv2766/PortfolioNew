3236873d4c1056bf5bb69780c9cb8ad83daf2f71
apoorv2766 <145976905+apoorv2766@users.noreply.github.com>
2023-12-03 16:19:49 +0530

    link added

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -98,7 +98,7 @@ const Work = () => {
                 ReactJS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="https://github.com/apoorv2766">
+                <a href="https://todocontextlocalusingvite.netlify.app">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
