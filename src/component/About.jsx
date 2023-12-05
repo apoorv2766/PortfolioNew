495d9df8543fbbf2d5ef37279fb2486aadff2a5c
apoorv2766 <145976905+apoorv2766@users.noreply.github.com>
2023-12-05 21:28:07 +0530

    added twiiter

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,28 +1,40 @@
 import React from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
               About
             </p>
           </div>
           <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 b-">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi. I'am Apoorv,nice to meet you.Please take a look around,</p>
           </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'am Apoorv,nice to meet you.Please take a look around,</p>
-            </div>
-            <div>
-              <p> I am passionate about building execellent software that improve
+          <div>
+            <p>
+              {" "}
+              I am passionate about building execellent software that improve
               the lives of those around me . I specialize in creating software .
               What would you do if you had a software expert avaliable at your
-              fingertip?</p>  
+              fingertip?
+            </p>
+            <div>
+              <a
+                className=" flex justify-between items-center w-full text-gray-300"
+                href="https://www.linkedin.com/in/apoorv27"
+              >
+                Linkedin <FaLinkedin size={30} />
+              </a>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
